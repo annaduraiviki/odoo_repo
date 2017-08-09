2dbf2ad67fecb88971c08db5f0d317f0ac95c15f
Annadurai <anna2durai@gmail.com>
2017-08-09 13:13:35 +0530

    Update __manifest__.py

--- customcountrystate/__manifest__.py
+++ customcountrystate/__manifest__.py
@@ -1,35 +1,27 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "country customfiles",
+    'name': "country custom fields",
 
     'summary': """
-       contains 2016 Custom files""",
+       Depends VMS vechicle management system country code""",
 
     'description': """
-        Long description of module's purpose
+        Used along with vechicle management system dependent. Study purpose
     """,
 
-    'author': "viki",
+    'author': "VA",
     'website': "http://viki2.odoo.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Custom',
+    'version': '10.0',
 
-    # any module necessary for this one to work correctly
     'depends': ['base',
                 ],
 
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
         'views/countrystate_view.xml',
         'views/base_countrystate_view.xml',
-        #'views/templates.xml',
     ],
-    # only loaded in demonstration mode
     'demo': [
         #'demo/demo.xml',
     ],
